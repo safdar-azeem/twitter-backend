94de3f54363d9e098dd0046362f619e0e7f4991b
safdar-azeem <safdarazeem00@gmail.com>
2021-04-03 19:00:00 +0000

    Add index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const connectDb = require('./config/connectDb.config')
+
+const app = express()
+
+app.use(
+   fileUpload({
+      useTempFiles: true,
+   })
+)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({ origin: '*' }))
+
+app.get('/', (req, res) => {
+   res.send('Welcome to the API')
+})
+
+app.use('/api/v1/', routes)
+
+const startServer = async () => {
+   const port = process.env.PORT || 5050
+   try {
+      await connectDb()
+      app.listen(port, () => {
+         console.log(`🚀  Server ready at: http://localhost:${port}`)
+      })
+   } catch (err) {
+      console.error(err.message)
+      process.exit(1)
+   }
+}
+
+startServer()
