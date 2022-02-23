13c6531cef55865df1b81815f62d8a588c40d871
safdar-azeem <safdarazeem00@gmail.com>
2022-02-24 03:14:44 +0500

    Feat : added auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const STATUS = require('../utils/status');
+const User = require('../models/user.model');
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+        const { id, email } = decodedToken;
+        const user = await User.find({ _id: id, email });
+        if (!user) {
+            return res.status(STATUS.NOT_FOUND).json({
+                status: STATUS.NOT_FOUND,
+                message: 'User not found'
+            });
+        } else {
+            req.user = user;
+            next();
+        } 
+    } catch (err) {
+        res.status(STATUS.UNAUTHORIZED).json({
+            status: STATUS.UNAUTHORIZED,
+            message: 'Invalid token'
+        });
+    }
+}
+
+module.exports = authMiddleware;
