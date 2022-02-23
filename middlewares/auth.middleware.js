8d75cff0e86c51186500ad713072a9ebff272ee4
safdar-azeem <safdarazeem00@gmail.com>
2022-02-24 03:31:25 +0500

    Feat : checking req.headers.authorization is true or it start with Bearer

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,6 +4,14 @@ const User = require('../models/user.model');
 
 const authMiddleware = async (req, res, next) => {
     try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || authHeader.split(' ')[0] !== 'Bearer') {
+            return res.status(STATUS.UNAUTHORIZED).json({
+                message: 'No token provided.'
+            });
+        } 
+
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
         const { id, email } = decodedToken;
