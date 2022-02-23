833a6909c91c1ee24cbe6f31abffe44d736d75e8
safdar-azeem <safdarazeem00@gmail.com>
2022-02-23 20:54:56 +0500

    Feat : added user model

---
+++ models/user.model.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function(value) {
+                if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+                    throw new Error('Email is not valid');
+                } 
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 20,
+        validate: {
+            validator: function(value) {
+                if (value.length < 6) {
+                    throw new Error({
+                        message: 'Password must be at least 6 characters long',
+                    });
+                } else if (value.length > 20) {
+                    throw new Error({
+                        message: 'Password must be less than 20 characters long',
+                    });
+                } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/.test(value)) {
+                    throw new Error({
+                        message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+                    });
+                }
+            },
+            } 
+    },
+    avatar: {
+        type: String
+    },
+    date_created: {
+        type: Date,
+        default: Date.now
+    },
+    total_Followers: {
+        type: Number,
+        default: 0
+    },
+    total_Following: {
+        type: Number,
+        default: 0
+    },
+    total_Tweets: {
+        type: Number,
+        default: 0
+    },
+    total_Notifications: {
+        type: Number,
+        default: 0
+    },
+    total_Messages: {
+        type: Number,
+        default: 0
+    },
+    is_Verified: {
+        type: Boolean,
+        default: false
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
