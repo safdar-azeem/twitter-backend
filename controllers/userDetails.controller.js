b8968c10103ab58d3c1724eb46638958a16e4d38
safdar-azeem <safdarazeem00@gmail.com>
2022-02-27 15:54:19 +0500

    Feat : created a route for edit user details

--- controllers/userDetails.controller.js
+++ controllers/userDetails.controller.js
@@ -1,5 +1,6 @@
 const controller = {};
 const STATUS = require('../utils/status');
+const User = require('../models/user.model');
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 
@@ -31,5 +32,37 @@ controller.uploadAvatar = async (req, res) => {
 		});
 	}
 };
+
+
+controller.editUserDetails = async (req, res) => {
+	try {
+		const { userId } = req.params;
+		let user = await User.findById(userId);
+		if (!user) {
+			return res.status(STATUS.NOT_FOUND).json({
+				status: STATUS.NOT_FOUND,
+				message: 'User not found',
+			});
+		}
+		
+		for (let key in req.body) {
+			user[key] = req.body[key];
+		}
+
+		await user.save();
+		
+		return res.status(STATUS.SUCCESS).json({
+			status: STATUS.SUCCESS,
+			message: 'User details updated successfully',
+			user,
+		});
+	} catch (err) {
+		return res.status(STATUS.INTERNAL_SERVER_ERROR).json({
+			status: STATUS.INTERNAL_SERVER_ERROR,
+			message: err.message,
+		});
+	}
+};
+
 
 module.exports = controller;
--- routes/userDetails.route.js
+++ routes/userDetails.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/userDetails.controller');
 
 router.post('/uploadAvatar', controller.uploadAvatar);
+router.post('/editUserDetails/:userId', controller.editUserDetails);
 
 module.exports = router;
 
