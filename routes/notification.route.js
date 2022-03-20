291f7667b782ef258942ef65cb21348584a6f856
safdar-azeem <safdarazeem00@gmail.com>
2022-03-20 19:15:01 +0500

    Feat : Created api for getNotifications, countUnSeenNotifications, markAsSeen , markAsRead

--- routes/notification.route.js
+++ routes/notification.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/comment.controller');
+const controller = require('../controllers/notification.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
-router.get('/', authMiddleware, controller.getNotifications);
+router.get('/get/:userId', authMiddleware, controller.getNotifications);
+router.get('/count/:userId', authMiddleware, controller.countUnSeenNotifications);
+router.put('/markAsSeen/:userId', authMiddleware, controller.markAsSeen);
+router.put('/markAsRead/:userId/:notificationId', authMiddleware, controller.markAsRead);
 
 module.exports = router;
